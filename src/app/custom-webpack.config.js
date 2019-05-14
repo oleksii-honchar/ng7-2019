d6e9395fd5605d9cfab9d29e88de3853b10df7d8
aleksey-gonchar <gonchar.aleksey@gmail.com>
2019-05-14 22:36:05 +0300

    * bindings

--- src/app/custom-webpack.config.js
+++ src/app/custom-webpack.config.js
@@ -6,13 +6,13 @@ module.exports = {
   // Use as a custom webpack builder in angular.json
   // https://webpack.js.org/configuration/output/#output-library
   output: {
-    library: 'appShared'
+    library: 'ng7Test'
   },
   plugins: [
     new WebpackShellPlugin({
       onBuildStart: [`echo "Building app [${$pkg.name}] bundle..."`],
       onBuildEnd: [
-        'sleep 3s && echo "touching app-svc to restart..." && touch ../app-svc/src/index.js',
+        `if [ "${process.env.SSR}" = "true" ]; then sleep 3s && echo "touching app-svc to restart..." && touch ../app-svc/src/index.js; fi`,
       ],
     }),
   ],
