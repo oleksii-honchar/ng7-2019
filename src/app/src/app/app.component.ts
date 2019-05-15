9cfbe10530f2f7474adcc0563f67d29ec08ba112
aleksey-gonchar <gonchar.aleksey@gmail.com>
2019-05-15 21:23:21 +0300

    * cleanup for project

--- src/app/src/app/app.component.ts
+++ src/app/src/app/app.component.ts
@@ -4,11 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  styles: [`
-    h1 {
-      color: dodgerblue;
-    }
-  `]
 })
 export class AppComponent {
   name = 'main';
