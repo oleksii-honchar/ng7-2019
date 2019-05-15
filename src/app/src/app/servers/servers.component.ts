4b984ef589ce56c3f54ce28e904710fd21df030e
aleksey-gonchar <gonchar.aleksey@gmail.com>
2019-05-15 21:21:36 +0300

    * directives

--- src/app/src/app/servers/servers.component.ts
+++ src/app/src/app/servers/servers.component.ts
@@ -3,18 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.scss']
+  styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Test';
   serverCreated = false;
+  servers = [
+    'Test1',
+    'Test2'
+  ];
+  isDetailsBtnClicked = false;
+  detailsBtnClickLogs = [];
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
-    }, 2000)
+    }, 2000);
   }
 
   ngOnInit() {
@@ -23,9 +29,16 @@ export class ServersComponent implements OnInit {
   onCreateServer () {
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
     this.serverCreated = true;
+    this.servers.push(this.serverName);
   }
 
   onUpdateServerName (event: any) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  toggleDetailsBtn () {
+    this.isDetailsBtnClicked = !this.isDetailsBtnClicked;
+
+    this.detailsBtnClickLogs.push(new Date())
   }
 }
