f5e53ff2c6f31cb4906e27a30cebd9c8647cf116
aleksey-gonchar <gonchar.aleksey@gmail.com>
2019-06-12 22:05:27 +0300

    section-51: * add model, @src path tslint and tsconfig from scm

--- src/app/src/app/modules/recipes/components/recipe-list/recipe-list.component.ts
+++ src/app/src/app/modules/recipes/components/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '@src/app/modules/recipes/models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'test recipe',
+      'some desc',
+      'https://prods3.imgix.net/images/articles/2017_04/Non-featured-Chorizo-egg-recipe.jpg?auto=format%2Ccompress&ixjsv=2.2.3&w=670')
+  ];
 
   constructor() { }
 
